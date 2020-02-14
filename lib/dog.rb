6a33574f9a17557d9403916185cefb872a203e3f
Sheriefs Gamble <sgamble284@gmail.com>
2020-02-14 10:42:31 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,54 @@
+class Dog
+  attr_accessor :id, :name, :breed
+  
+  def initialize(attributes)
+    @id = id
+    @name = name
+    @breed = breed
+    self.id ||= nil
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+        
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql = "DROP TABLE dogs"
+    
+    DB[:conn].execute(sql)
+  end
+  
+  def self.new_from_db(row)
+    attributes = {
+      id: row[0],
+      name: row[1],
+      breed: row[2]
+    }
+    self.new(attributes)
+  end
+  
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE name = ?
+    SQL
+    
+    DB[:conn].execute(sql, name).map do |row|
+      new_from_db(row)
+    end.first
+  end
+  
+  def update
+    
+  end
+  
+end
