6c7fa1028dac523240448d854851d3f2325632c7
Sheriefs Gamble <sgamble284@gmail.com>
2020-02-14 11:04:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -57,7 +57,13 @@ class Dog
     if self.id
       self.update
     else
-      
+      sql = <<-SQL
+      INSERT INTO songs (name, breed)
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+  end
   end
   
 end
